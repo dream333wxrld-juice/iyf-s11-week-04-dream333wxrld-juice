4716b4346fc3201227bb08351c522d0e13802f16
Gilbert <dream333wxrld@gmail.com>
2026-07-07 23:25:04 +0300

    feat: add Task 7.1 JS setup and variables

---
+++ main.js
@@ -0,0 +1,26 @@
+// Variable declarations
+let name = "Gilbert Mungai";
+let age = 18;
+const birthYear = 2007;
+
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;
+
+const PI = 3.14159;
+
+// Variable Practice
+let isStudent = true;
+let favoriteColors = ["black", "orange", "white"];
+let today = new Date();
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Favorite Colors:", favoriteColors);
+console.log("Today's Date:", today);

55f1520cce3f0c5cabbce150c90caa793732d359
Gilbert <dream333wxrld@gmail.com>
2026-07-09 23:07:06 +0300

    feat: add Task 7.4 Exercise 2 switch statements

--- main.js
+++ main.js
@@ -175,3 +175,31 @@ console.log(getGrade(85));  // B
 console.log(getGrade(75));  // C
 console.log(getGrade(65));  // D
 console.log(getGrade(50));  // F
+// Task 7.4: Control Flow - Exercise 2: Switch Statements
+
+function getDayName(dayNumber) {
+    switch (dayNumber) {
+        case 0:
+            return "Sunday";
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        default:
+            return "Invalid day";
+    }
+}
+
+// Test
+console.log(getDayName(0));  // Sunday
+console.log(getDayName(1));  // Monday
+console.log(getDayName(3));  // Wednesday
+console.log(getDayName(7));  // Invalid day
